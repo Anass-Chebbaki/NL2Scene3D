8c24e2d5d9a6a4e6385e0ba4a4271adacff2e249
Anass-Chebbaki <S1099007@studenti.univpm.it>
2026-06-02 15:44:51 +0200

    clean push on the new branch

--- src/nl2scene3d_addon/nl2scene3d/blender/__init__.py
+++
@@ -1,6 +0,0 @@
-# src/nl2scene3d/blender/__init__.py
-"""
-Sub-package for modules that require the Blender environment (bpy).
-All modules in this sub-package are designed to run exclusively
-inside Blender's embedded Python interpreter.
-"""
--- src/nl2scene3d_addon/nl2scene3d/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-# nl2scene3d/utils/__init__.py
-"""
-Utility sub-package: geometry and collision helpers for the NL2Scene3D pipeline.
-"""
